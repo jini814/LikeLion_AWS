05f3d9f2caf0563f0051cb033b90720d149a39a1
jini <rose7734@naver.com>
2024-06-12 11:08:31 +0900

    추가

--- src/main/java/org/example/todoapp/entity/User.java
+++ src/main/java/org/example/todoapp/entity/User.java
@@ -5,12 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Setter
-@Getter
+@Setter@Getter
 @Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
--- src/main/java/org/example/todoapp/repository/TodoRepository.java
+++ src/main/java/org/example/todoapp/repository/TodoRepository.java
@@ -1,9 +1,9 @@
 package org.example.todoapp.repository;
 
-import org.example.todoapp.entity.User;
+import org.example.todoapp.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface TodoRepository extends JpaRepository<Todo,Long> {
 }
